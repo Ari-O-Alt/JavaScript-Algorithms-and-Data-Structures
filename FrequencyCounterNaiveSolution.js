9a1fd0a776c300ec5620d9b07754f24ff831e457
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2022-12-18 12:40:55 +0100

    Add naive solution for Frequency Counter

---
+++ FrequencyCounterNaiveSolution.js
@@ -0,0 +1,43 @@
+/* Problem: build a function that taxes two strings an in input and determines if for every value in the first array, there's a squared correspondent value in the second array. The frequnecy is important so this means if we have 3 values of 3 in the first array, we should have exactly 3 values of 9 in the second.
+ */
+// Naive approach - O(n*n)
+
+const frequencyCounter = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let value of array1) {
+    const correspondentIndex = array2.indexOf(value ** 2);
+    if (correspondentIndex === -1) {
+      return false;
+    } else {
+      array2.splice(correspondentIndex, 1);
+    }
+  }
+
+  return true;
+};
+
+console.log(frequencyCounter([1, 2, 3, 3], [1, 4, 9, 9])); // returns true
+console.log(frequencyCounter([1, 2, 3, 3], [1, 4, 9])); // returns false
+
+// TYPESCRIP VERSION
+
+/* const frequencyCounter = (array1: [], array2: []): boolean => {
+    if (array1.length !== array2.length) {
+      return false 
+    }
+    
+    for (let value of array1) {
+      const correspondentIndex: number = array2.indexOf(value ** 2);
+      if (correspondentIndex === -1) {
+       return false;
+      } else {
+       array2.splice(correspondentIndex, 1);
+      }
+  
+    }
+    
+    return true
+  } */
