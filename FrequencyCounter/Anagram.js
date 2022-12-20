b074cba3f9a000ac6fe1757130c76ba69d3191b4
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2022-12-20 21:36:21 +0100

    Finshed areStringsAnagrams function

--- FrequencyCounter/Anagram.js
+++ FrequencyCounter/Anagram.js
@@ -1,6 +1,67 @@
 /**
  * Create a function that given two strings, it determines if they are anagrams or not
  * Anagram = a word or a phase which can be formed by rearranging the letters of another word or phrase
+ * For this function, we can asume the input is only one word strings, lowercase, no punctuation (so no white spaces, question marks, quotes etc)
  */
 
-const areStringsAnagrams = (string1, string2) => {};
+const areStringsAnagrams = (string1, string2) => {
+  if (string1.length !== string2.length) {
+    return false;
+  }
+
+  const string1AsObject = {};
+  const string2AsObject = {};
+
+  for (let value of string1) {
+    string1AsObject[value] = (string1AsObject[value] || 0) + 1;
+  }
+
+  for (let secondValue of string2) {
+    string2AsObject[secondValue] = (string2AsObject[secondValue] || 0) + 1;
+  }
+
+  for (let property in string1AsObject) {
+    if (!string2AsObject[property]) {
+      return false;
+    }
+    if (string1AsObject[property] !== string2AsObject[property]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// TESTS
+console.log(areStringsAnagrams("banana", "banaan")); // returns true
+console.log(areStringsAnagrams("", "")); // returns true
+console.log(areStringsAnagrams("banana", "bananaaaaa")); // return false
+
+// TYPESCRIPT VERSION
+/* const areStringsAnagrams = (string1: string, string2: string): boolean => {
+  if (string1.length !== string2.length) {
+    return false;
+  }
+
+  const string1AsObject = {};
+  const string2AsObject = {};
+
+  for (let value of string1) {
+    string1AsObject[value] = (string1AsObject[value] || 0) + 1;
+  }
+
+  for (let secondValue of string2) {
+    string2AsObject[secondValue] = (string2AsObject[secondValue] || 0) + 1;
+  }
+
+  for (let property in string1AsObject) {
+    if (!string2AsObject[property]) {
+      return false;
+    }
+    if (string1AsObject[property] !== string2AsObject[property]) {
+      return false;
+    }
+  }
+
+  return true;
+}; */
--- FrequencyCounter/FrequencyCounterRefactoredVersion.js
+++ FrequencyCounter/FrequencyCounterRefactoredVersion.js
@@ -1,7 +1,7 @@
 /* Problem: build a function that takes two strings an in input and determines if for every value in the first array, there's a squared correspondent value in the second array. The frequnecy is important so this means if we have 2 values of 3 in the first array, we should have exactly 2 values of 9 in the second.
  */
 
-// Refactored approach - O(1)
+// Refactored approach - O(n)
 const frequencyCounter = (array1, array2) => {
   if (array1.length !== array2.length) {
     return false;
