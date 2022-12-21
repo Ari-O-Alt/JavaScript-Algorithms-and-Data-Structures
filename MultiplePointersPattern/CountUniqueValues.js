c11e388315c1cb60da588f4b99302dae04d3962e
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2022-12-21 09:31:05 +0100

    Start working on countUniqueValues()

---
+++ MultiplePointersPattern/CountUniqueValues.js
@@ -0,0 +1,12 @@
+/**
+ * Create a function that takes a sorted array of number as input and returns the number of uniqe values fund in that array
+ */
+
+const countUniqueValues = () => {};
+
+// TESTS
+countUniqueValues([1, 2]); // returns 2
+countUniqueValues([]); // returns 0
+countUniqueValues([1, 1, 1, 1, 1, 1]); // returns 1
+
+// TYPESCRIPT VERSION
