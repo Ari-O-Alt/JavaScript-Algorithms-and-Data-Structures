973453a199417bc4cf6402132efaee8dfe94cac9
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2022-12-24 19:38:13 +0100

    Renamed amd finished buildAndCountNumberInstances()

---
+++ MultiplePointersPattern/BuildAndCountNumberInstances.js
@@ -0,0 +1,39 @@
+/**
+ * Create a function that takes a sorted array of numbers as input and builds and returns the number of values fund in that array
+ */
+
+const buildAndCountNumberInstances = (arrayOfNumbers) => {
+  const firstPointer = 0;
+
+  for (
+    let secondPointer = 1;
+    secondPointer < arrayOfNumbers.length;
+    secondPointer++
+  ) {
+    if (arrayOfNumbers[firstPointer] !== arrayOfNumbers[secondPointer]) {
+      firstPointer++;
+      arrayOfNumbers[firstPointer] = arrayOfNumbers[secondPointer];
+    }
+  }
+
+  return firstPointer + 1;
+};
+
+// TESTS
+buildAndCountNumberInstances([1, 2]); // returns 2
+buildAndCountNumberInstances([]); // returns 0
+buildAndCountNumberInstances([1, 1, 1, 1, 1, 1]); // returns 1
+
+// TYPESCRIPT VERSION
+/* const buildAndCountUniqueInstances = (arrayOfNumbers: number[]): number => {
+  const firstPointer: number = 0;
+  
+  for (let secondPointer = 1; secondPointer < arrayOfNumbers.length; secondPointer++) {
+      if (arrayOfNumbers[firstPointer] !== arrayOfNumbers[secondPointer]) {
+       firstPointer++
+       arrayOfNumbers[firstPointer] = arrayOfNumbers[secondPointer]
+      } 
+  }
+  
+  return firstPointer + 1;
+}; */
