525a723651199d054b5c17ab67e5e3f5596adc47
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2022-12-28 18:19:06 +0100

    Add areThereDuplicates(), two pointer solution

--- Section6OptionalChallenges/FrequencyCounter-AreThereDuplicates.js
+++ Section6OptionalChallenges/FrequencyCounter-AreThereDuplicates.js
@@ -2,12 +2,55 @@
  * Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
  */
 
-const areThereDuplicates = () => {};
+// FREQUENCY COUNTER SOLUTION
+const areThereDuplicates = (arg1, arg2, arg3, ...n) => {
+  if (arguments.length === 0) {
+    return "false";
+  }
+
+  const argumentsAsObjects = {};
+
+  for (let value of arguments) {
+    argumentsAsObjects[value] = (argumentsAsObjects[value] || 0) + 1;
+  }
+
+  console.log(argumentsAsObjects);
+
+  for (let property in argumentsAsObjects) {
+    if (argumentsAsObjects[property] > 1) {
+      return true;
+    }
+
+    return false;
+  }
+};
+
+// MULTIPLE POINTERS SOLUTION
 
 // TESTS
-
 areThereDuplicates(1, 2, 3); // returns false
 areThereDuplicates(1, 2, 2); // returns true
 areThereDuplicates("a", "b", "c", "a"); // returns true
 
-// TYPESCRIPT VERSION
+// FREQUENCY COUNTER SOLUTION - TYPESCRIPT VERSION
+/* const areThereDuplicates = (arg1, arg2, arg3, ...n: number [] | string[]): boolean => {
+    if (arguments.length === 0) {
+      return false;
+    }
+  
+    const argumentsAsObjects = {};
+  
+    for (let value of arguments) {
+      argumentsAsObjects[value] = (argumentsAsObjects[value] || 0) + 1;
+    }
+  
+    for (let property in argumentsAsObjects) {
+      if (argumentsAsObjects[property] > 1) {
+        return true;
+      }
+  
+      return false;
+    }
+  };
+ */
+// MULTIPLE POINTERS SOLUTION - TYPESCRIPT VERSION
