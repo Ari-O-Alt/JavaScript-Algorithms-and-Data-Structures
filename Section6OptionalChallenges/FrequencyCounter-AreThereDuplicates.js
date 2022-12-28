46438adcbfc3adb879d160ba572e3abdff28ddfd
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2022-12-28 17:49:16 +0100

    Add and start working on areThereDuplicates()

---
+++ Section6OptionalChallenges/FrequencyCounter-AreThereDuplicates.js
@@ -0,0 +1,13 @@
+/**
+ * Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+ */
+
+const areThereDuplicates = () => {};
+
+// TESTS
+
+areThereDuplicates(1, 2, 3); // returns false
+areThereDuplicates(1, 2, 2); // returns true
+areThereDuplicates("a", "b", "c", "a"); // returns true
+
+// TYPESCRIPT VERSION
